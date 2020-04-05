9622488abb597a0edf9aade5c09037e4374c0119
Stefan <stefanvanderheijden@gmail.com>
2020-04-05 11:53:37 +0200

    Added player list

--- client/src/client.js
+++ client/src/client.js
@@ -12,6 +12,27 @@ const writeEvent = (text) => {
 
 };
 
+class CreatePlayer {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+// Create all the players with their coordinates on the canvas
+// Note that users that login have yet to be linked to a player
+
+P1 = new CreatePlayer(16,16);
+P2 = new CreatePlayer(56,16);
+P3 = new CreatePlayer(96,16);
+P4 = new CreatePlayer(136,16);
+P5 = new CreatePlayer(136,64);
+P6 = new CreatePlayer(136,112);
+P7 = new CreatePlayer(96,112);
+P8 = new CreatePlayer(56,112);
+P9 = new CreatePlayer(16,112);
+P10 = new CreatePlayer(16,64);
+
 //initialize canvas
 const canvas = document.getElementById("my-canvas")
 const context = canvas.getContext("2d")
@@ -27,7 +48,7 @@ longhair.src = "src/charhairlong.png"
 shocked.src = "src/shocked.png"
 bg.onload = () => {
   context.drawImage(bg, 0, 0)
-  context.drawImage(char, 16, 16)
+  context.drawImage(char, P1.x, P1.y)
   context.drawImage(longhair, 16, 16)
   context.drawImage(shocked, 16, 16)
 }
